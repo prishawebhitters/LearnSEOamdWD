dc7506951761576cc9640f750d943b4fb3f6cad9
webhitters0123 <xyz.webhitters@gmail.com>
2025-01-23 10:22:58 +0530

    changes

--- script.js
+++ script.js
@@ -294,4 +294,21 @@ setTimeout(showPopup, 10000);
             generate();
         }
     }
-    
+//---------FAQ----------
+const btns = document.querySelectorAll(".acc-btn");
+
+// fn
+function accordion() {
+  // this = the btn | icon & bg changed
+  this.classList.toggle("is-open");
+
+  // the acc-content
+  const content = this.nextElementSibling;
+
+  // IF open, close | else open
+  if (content.style.maxHeight) content.style.maxHeight = null;
+  else content.style.maxHeight = content.scrollHeight + "px";
+}
+
+// event
+btns.forEach((el) => el.addEventListener("click", accordion));
